ba5939e06c18d56fcd9f1e4267937367d1b68e59
Nick Mckenzie <nicholas.mckenzie@mcb.mu>
2020-05-11 12:35:47 +0400

    Fix race condition

--- JavaScriptSPA/ui.js
+++ JavaScriptSPA/ui.js
@@ -55,52 +55,25 @@ function updateUI(data, endpoint) {
     
   } else if (endpoint == graphConfig.graphMeetingEndpoint){
 
+    /*
     const resultsCard = document.getElementById("resultsCard");
     const meetingLink = document.createElement('a');
     meetingLink.setAttribute("href", data.joinWebUrl);
     meetingLink.innerText = "Teams Meeting";
     resultsCard.appendChild(meetingLink);
+    */
 
-    /*
-    var linkHtml = decodeURIComponent(joinInformation.content.split(",")[1]);
+    
+    var linkHtml = decodeURIComponent(data.joinInformation.content)
+    .replace(/\+/g," ")
+    .replace("data:text/html,","")
+    .trim();
     const linkContent =document.createElement('div');
     linkContent.innerHTML = linkHtml;
     results.appendChild(linkContent);
-    */
+    
 
     results.classList.remove('d-none');
     
-  }
-  else if (endpoint === graphConfig.graphMailEndpoint) {
-      if (data.value.length < 1) {
-        alert("Your mailbox is empty!")
-      } else {
-        const tabList = document.getElementById("list-tab");
-        tabList.innerHTML = ''; // clear tabList at each readMail call
-        const tabContent = document.getElementById("nav-tabContent");
-
-        data.value.map((d, i) => {
-          // Keeping it simple
-          if (i < 10) {
-            const listItem = document.createElement("a");
-            listItem.setAttribute("class", "list-group-item list-group-item-action")
-            listItem.setAttribute("id", "list" + i + "list")
-            listItem.setAttribute("data-toggle", "list")
-            listItem.setAttribute("href", "#list" + i)
-            listItem.setAttribute("role", "tab")
-            listItem.setAttribute("aria-controls", i)
-            listItem.innerHTML = d.subject;
-            tabList.appendChild(listItem)
-    
-            const contentItem = document.createElement("div");
-            contentItem.setAttribute("class", "tab-pane fade")
-            contentItem.setAttribute("id", "list" + i)
-            contentItem.setAttribute("role", "tabpanel")
-            contentItem.setAttribute("aria-labelledby", "list" + i + "list")
-            contentItem.innerHTML = "<strong> from: " + d.from.emailAddress.address + "</strong><br><br>" + d.bodyPreview + "...";
-            tabContent.appendChild(contentItem);
-          }
-        });
-      }
   }
 }
